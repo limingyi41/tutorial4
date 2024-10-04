9a9bdf8506c128d6c13c0df19eadc80647296457
limingyi41 <limingyi41.gmail.com>
2024-10-05 00:55:48 +0800

    Added testSubtraction

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,19 +1,13 @@
+
 package org.example;
 
 public class Calc {
 
-    public int add(int x, int y)
-    {
-        return x+y;
+    public int add(int x, int y) {
+        return x + y;
     }
 
-    public int subtract(int x, int y)
-    {
-        return x*y;
+    public int subtract(int x, int y) {
+        return x - y; // 修正为减法
     }
-}
-@Test
-public void testSubtraction() {
-    Calc c = new Calc();
-    assertEquals(2, c.subtract(4, 2));
 }
